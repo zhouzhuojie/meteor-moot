01e4a2e86e97fd871266b5d64a3d29f5eccd711e
zhouzhuojie <zhouzhuojie@gmail.com>
2014-09-17 19:00:41 -0400

    Add moot.css

--- package.js
+++ package.js
@@ -7,6 +7,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0', 'client');
+  api.add_files('moot/moot.css', 'client');
   api.add_files('moot/moot.min.js', 'client');
 });
 
